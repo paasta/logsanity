58077617873124248bd03be0037ee950101031a5
Jonas Pfenniger <jonas@pfenniger.name>
2013-06-20 23:04:20 +0100

    Configure ES with a logstash template

--- recipes/logstash.rb
+++ recipes/logstash.rb
@@ -1,2 +1,12 @@
 include_recipe 'logstash::server'
 include_recipe 'logstash::index_cleaner'
+
+template_path = "#{node['logstash']['base_dir']}/logstash-template.json"
+
+template template_path do
+  mode "0644"
+end
+
+execute "install-logstash-template" do
+  command "curl -XPUT 'http://localhost:9200/_template/logstash/' -d @#{template_path}"
+end
