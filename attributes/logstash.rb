6fb32ae3abb39d4c4f89573ebe292eadeb03a031
Jonas Pfenniger <jonas@pfenniger.name>
2013-06-20 20:40:42 +0100

    Logstash config I/F/O fixups

--- attributes/logstash.rb
+++ attributes/logstash.rb
@@ -1,10 +1,7 @@
 set['logstash']['index_cleaner']['days_to_keep'] = 7
 set['logstash']['server']['install_rabbitmq']    = false
 set['logstash']['server']['enable_embedded_es']  = false
-set['logstash']['server']['outputs'] = [{
-  elasticsearch: {
-    embedded: false,
-    cluster: "logstash",
-    host: "localhost",
-  }
-}]
+set['logstash']['elasticsearch_ip'] = '127.0.0.1'
+set['logstash']['elasticsearch_cluster'] = node['elasticsearch']['cluster']['name']
+set['logstash']['server']['inputs'] = []
+set['logstash']['server']['filters'] = []
