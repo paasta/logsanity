09eec41a942a0beebb0260f53b1992170d8e09dd
Jonas Pfenniger <jonas@pfenniger.name>
2013-06-21 14:58:47 +0100

    Attribute set override event the chef-solo json. Avoid doing that.

--- attributes/logstash.rb
+++ attributes/logstash.rb
@@ -3,5 +3,3 @@ set['logstash']['server']['install_rabbitmq']    = false
 set['logstash']['server']['enable_embedded_es']  = false
 set['logstash']['elasticsearch_ip'] = '127.0.0.1'
 set['logstash']['elasticsearch_cluster'] = node['elasticsearch']['cluster']['name']
-set['logstash']['server']['inputs'] = []
-set['logstash']['server']['filters'] = []
