def87d6832297390de7df825bd7ddfa444952989
Jonas Pfenniger <jonas@pfenniger.name>
2013-07-26 18:51:50 +0100

    Disable multicast discovery on EC2

--- attributes/elasticsearch.rb
+++ attributes/elasticsearch.rb
@@ -2,3 +2,4 @@ include_attribute 'base::default'
 
 set['elasticsearch']['cluster']['name'] = 'logsanity'
 set['elasticsearch']['node']['name'] = node['base']['hostname']
+set['elasticsearch']['discovery']['zen']['ping']['multicast']['enabled'] = false if node['ec2']
