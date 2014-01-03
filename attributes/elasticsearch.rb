281c9bbf66652d84d128890b1ec5ee066939f8b6
zimbatm <zimbatm@zimbatm.com>
2014-01-03 11:36:38 +0000

    Versions updates

--- attributes/elasticsearch.rb
+++ attributes/elasticsearch.rb
@@ -1,5 +1,9 @@
 include_attribute 'base::hostname'
 
 set['elasticsearch']['cluster']['name'] = 'logsanity'
-set['elasticsearch']['node']['name'] = node['base']['hostname']
+set['elasticsearch']['node']['name']    = node['base']['hostname']
 set['elasticsearch']['discovery']['zen']['ping']['multicast']['enabled'] = false if node['ec2']
+set['elasticsearch']['plugins']['elasticsearch/elasticsearch-cloud-aws']['version'] = '1.16.0'
+set['elasticsearch']['version']         = "0.90.9"
+set['elasticsearch']['filename']        = "elasticsearch-#{node.elasticsearch[:version]}.tar.gz"
+set['elasticsearch']['download_url']    = [node.elasticsearch[:host], node.elasticsearch[:repository], node.elasticsearch[:filename]].join('/')
