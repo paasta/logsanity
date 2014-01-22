1ce6e1e03e4e3cda04870492e3dbe65bd18d1319
zimbatm <zimbatm@zimbatm.com>
2014-01-22 19:11:38 +0000

    Updates the bigdesk plugin to v2.2.2

--- recipes/elasticsearch.rb
+++ recipes/elasticsearch.rb
@@ -8,7 +8,7 @@ include_recipe 'elasticsearch'
 include_recipe 'elasticsearch::aws' if node['ec2']
 
 install_plugin 'royrusso/elasticsearch-HQ'
-install_plugin 'lukas-vlcek/bigdesk/2.2.1'
+install_plugin 'lukas-vlcek/bigdesk/2.2.2'
 
 # Setup the logstash index cleaner
 
